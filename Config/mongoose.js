2d2bc4539bf164d477428059bdc7f12213752078
AmanLM <mullaaman2001@gmail.com>
2022-05-30 14:28:44 +0530

    hosting on heroku

--- Config/mongoose.js
+++ Config/mongoose.js
@@ -13,7 +13,7 @@
 
 
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/contact_list_db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/contact_list_db');
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error bind to db'));
 db.once('open',function(){
